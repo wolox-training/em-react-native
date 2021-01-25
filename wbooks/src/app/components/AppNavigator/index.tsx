2043930650c1b950d43f4e675756911f5ccba899
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2021-01-25 17:14:07 -0300

    Replaced route names... again

--- wbooks/src/app/components/AppNavigator/index.tsx
+++ wbooks/src/app/components/AppNavigator/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import BookList from '@screens/BookList';
 import BookDetail from '@screens/BookDetail';
+import { ROUTES } from '@constants/routes';
 
 import { SCREEN_OPTS } from './constants';
 
@@ -10,8 +11,8 @@ const Stack = createStackNavigator();
 function AppNavigator() {
   return (
     <Stack.Navigator screenOptions={SCREEN_OPTS as object}>
-      <Stack.Screen name="BookList" component={BookList} options={{ title: 'Listado de libros' }} />
-      <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: 'Detalle' }} />
+      <Stack.Screen name={ROUTES.BookList} component={BookList} options={{ title: 'Listado de libros' }} />
+      <Stack.Screen name={ROUTES.BookDetail} component={BookDetail} options={{ title: 'Detalle' }} />
     </Stack.Navigator>
   );
 }
